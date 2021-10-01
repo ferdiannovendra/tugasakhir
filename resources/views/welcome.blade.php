5e371c798d51e4be40360147757322c2310fb037
ferdiannovendra <ferdiannovendra15@gmail.com>
2021-10-01 14:37:19 +0700

    setup multitenancy done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,7 +44,9 @@
                         </g>
                     </svg>
                 </div>
-
+                @if(\Spatie\Multitenancy\Models\Tenant::checkCurrent())
+                <p class="bg-white">{{app('currentTenant')->name}}</p>
+                @endif
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
